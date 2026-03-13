15a4a05d8bc5bae65c71a97705116d9c0d97a9da
Linniel Driver-Williams <LinnielDW@googlemail.com>
2026-03-13 21:51:48 +0100

    implemented phoenix pride

--- src/Relics/PhoenixPride.cs
+++ src/Relics/PhoenixPride.cs
@@ -1,14 +1,50 @@
 ﻿using BaseLib.Abstracts;
 using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.RelicPools;
+using MegaCrit.Sts2.Core.ValueProps;
 
 namespace ArqPhoenixAncient.Relics;
 
 //TODO: IMPL
 // - Gain 1 extra energy per turn but at the start of your turn take 2 unavoidable damage
 [Pool(typeof(EventRelicPool))]
-public class PhoenixPride() : CustomRelicModel
+public class PhoenixPride : CustomRelicModel
 {
     public override RelicRarity Rarity => RelicRarity.Ancient;
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars
+    {
+        get
+        {
+            return new List<DynamicVar>([
+                new EnergyVar(1),
+                new HpLossVar(2)
+            ]);
+        }
+    }
+    
+    public override decimal ModifyMaxEnergy(Player player, decimal amount)
+    {
+        if (player != Owner)
+        {
+            return amount;
+        }
+        return amount + DynamicVars.Energy.IntValue;
+    }
+    
+    public override async Task BeforeHandDraw(Player player, PlayerChoiceContext choiceContext, CombatState combatState)
+    {
+        if (player == Owner)
+        {
+            Flash();
+            await CreatureCmd.Damage(choiceContext, Owner.Creature, DynamicVars.HpLoss.BaseValue, ValueProp.Unblockable, Owner.Creature);
+        }
+    }
+
 }
