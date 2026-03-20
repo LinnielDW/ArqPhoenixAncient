1de16d58ce804abdaf5f9e524cdb02a0203d8b7f
Linniel Driver-Williams <LinnielDW@googlemail.com>
2026-03-20 14:15:20 +0100

    added skeleton for Devistation card

---
+++ src/Cards/Devastation.cs
@@ -0,0 +1,23 @@
+﻿using BaseLib.Abstracts;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+
+namespace ArqPhoenixAncient.Cards;
+
+public class Devastation() : CustomCardModel(2,
+    CardType.Attack,
+    CardRarity.Ancient,
+    TargetType.AllEnemies)
+{
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        
+    }
+
+    protected override void OnUpgrade()
+    {
+
+    }
+}
