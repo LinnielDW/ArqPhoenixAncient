08c171532e95cf837f112594571e24d0f21ec463
Linniel Driver-Williams <LinnielDW@googlemail.com>
2026-03-21 11:36:34 +0100

    implemented Devastation

--- src/Cards/Devastation.cs
+++ src/Cards/Devastation.cs
@@ -1,23 +1,52 @@
 ﻿using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.ValueProps;
 
 namespace ArqPhoenixAncient.Cards;
 
-public class Devastation() : CustomCardModel(2,
+// 1 cost. Take 10 unavoidable damage. Deal 48 damage to ALL enemies. Exhaust
+[Pool(typeof(EventCardPool))]
+public class Devastation() : CustomCardModel(
+    1,
     CardType.Attack,
     CardRarity.Ancient,
     TargetType.AllEnemies)
 {
-    protected override async Task OnPlay(
-        PlayerChoiceContext choiceContext,
-        CardPlay play)
+    public override IEnumerable<CardKeyword> CanonicalKeywords =>
+    [
+        CardKeyword.Exhaust
+    ];
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars
     {
+        get
+        {
+            return new List<DynamicVar>(new DynamicVar[]
+            {
+                new DamageVar(48, ValueProp.Move),
+                new HpLossVar(8)
+            });
+        }
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        VfxCmd.PlayOnCreatureCenter(Owner.Creature, "vfx/vfx_bloody_impact");
+        await CreatureCmd.Damage(choiceContext, Owner.Creature, DynamicVars.HpLoss.BaseValue, ValueProp.Unblockable | ValueProp.Unpowered | ValueProp.Move, this);
         
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this)
+            .TargetingAllOpponents(CombatState)
+            .Execute(choiceContext);
     }
 
     protected override void OnUpgrade()
     {
-
+        DynamicVars.Damage.UpgradeValueBy(4);
+        DynamicVars.HpLoss.UpgradeValueBy(-3);
     }
 }
--- src/Relics/PhoenixTorch.cs
+++ src/Relics/PhoenixTorch.cs
@@ -1,16 +1,37 @@
-﻿using BaseLib.Abstracts;
+﻿using ArqPhoenixAncient.Cards;
+using BaseLib.Abstracts;
 using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.RelicPools;
 
 namespace ArqPhoenixAncient.Relics;
 
-//TODO: IMPL
-// add a card to deck. 
 [Pool(typeof(EventRelicPool))]
 public class PhoenixTorch : CustomRelicModel
 {
     public override RelicRarity Rarity => RelicRarity.Ancient;
 
-    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips
+    {
+        get
+        {
+            return HoverTipFactory.FromCardWithCardHoverTips<Devastation>();
+        }
+    }
+
+    public override async Task AfterObtained()
+    {
+        List<CardPileAddResult> results = [];
+        for (var i = 0; i < 2; i++)
+        {
+            CardModel cardModel = Owner.RunState.CreateCard<Devastation>(Owner);
+            var cardPileAddResult = await CardPileCmd.Add(cardModel, PileType.Deck);
+            results.Add(cardPileAddResult);
+        }
+        CardCmd.PreviewCardPileAdd(results, 2f);
+    }
 }
